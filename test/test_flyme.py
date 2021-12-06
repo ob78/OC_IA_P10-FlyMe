006b278a9b4c52f75cf9a799bca414e307eb7132
ob78 <61279321+ob78@users.noreply.github.com>
2021-12-06 15:55:24 +0100

    Update test_flyme.py

--- test/test_flyme.py
+++ test/test_flyme.py
@@ -6,14 +6,19 @@ CONFIG = DefaultConfig()
 
 
 def test_greetings_intent():
+    """
+    runtime_credentials = CognitiveServicesCredentials(CONFIG.LUIS_API_KEY)
+    #client_runtime = LUISRuntimeClient(endpoint=CONFIG.LUIS_API_ENDPOINT, credentials=runtime_credentials)
+    client_runtime = LUISRuntimeClient(CONFIG.LUIS_API_HOST_NAME, credentials=runtime_credentials)
+
     client_runtime = LUISRuntimeClient(
         CONFIG.LUIS_API_HOST_NAME,
         CognitiveServicesCredentials(CONFIG.LUIS_API_KEY))
-    """    
-    runtime_credentials = CognitiveServicesCredentials(CONFIG.LUIS_API_KEY)
-    client_runtime = LUISRuntimeClient(endpoint=CONFIG.LUIS_API_ENDPOINT, credentials=runtime_credentials)
     """
     
+    runtime_credentials = CognitiveServicesCredentials(CONFIG.LUIS_API_KEY)
+    client_runtime = LUISRuntimeClient(endpoint=CONFIG.LUIS_API_ENDPOINT, credentials=runtime_credentials)
+
     test_request = "Hello"
     test_response = client_runtime.prediction.resolve(CONFIG.LUIS_APP_ID, query=test_request)
 
@@ -21,26 +26,11 @@ def test_greetings_intent():
     actual_intent = test_response.top_scoring_intent.intent
     assert actual_intent == expected_intent
 
-    """
-    client_runtime = LUISRuntimeClient(endpoint=CONFIG.LUIS_API_ENDPOINT, credentials=runtime_credentials)
-
-    test_request = { "query" : "Hello" }
-    test_response = client_runtime.prediction.get_slot_prediction(CONFIG.LUIS_APP_ID, "Production", test_request)
-
-    expected_intent = "GreetingsIntent"
-    actual_intent = test_response.prediction.top_intent
-
-    assert actual_intent == expected_intent
-    """
 
 def test_none_intent():
-    client_runtime = LUISRuntimeClient(
-        CONFIG.LUIS_API_HOST_NAME,
-        CognitiveServicesCredentials(CONFIG.LUIS_API_KEY))
-    """    
     runtime_credentials = CognitiveServicesCredentials(CONFIG.LUIS_API_KEY)
     client_runtime = LUISRuntimeClient(endpoint=CONFIG.LUIS_API_ENDPOINT, credentials=runtime_credentials)
-    """
+
     test_request = "I want to rent a car"
     test_response = client_runtime.prediction.resolve(CONFIG.LUIS_APP_ID, query=test_request)
 
@@ -48,44 +38,14 @@ def test_none_intent():
     actual_intent = test_response.top_scoring_intent.intent
     assert actual_intent == expected_intent
 
-    """
-    runtime_credentials = CognitiveServicesCredentials(CONFIG.LUIS_API_KEY)
-    client_runtime = LUISRuntimeClient(endpoint=CONFIG.LUIS_API_ENDPOINT, credentials=runtime_credentials)
-
-    test_request = { "query" : "I want to rent a car" }
-    test_response = client_runtime.prediction.get_slot_prediction(CONFIG.LUIS_APP_ID, "Production", test_request)
-
-    expected_intent = "NoneIntent"
-    actual_intent = test_response.prediction.top_intent
-
-    assert actual_intent == expected_intent
-    """
 
 def test_order_travel_intent():
-    client_runtime = LUISRuntimeClient(
-        CONFIG.LUIS_API_HOST_NAME,
-        CognitiveServicesCredentials(CONFIG.LUIS_API_KEY))
-    """    
     runtime_credentials = CognitiveServicesCredentials(CONFIG.LUIS_API_KEY)
     client_runtime = LUISRuntimeClient(endpoint=CONFIG.LUIS_API_ENDPOINT, credentials=runtime_credentials)
-    """
-    
+
     test_request = "I need to book a flight"
     test_response = client_runtime.prediction.resolve(CONFIG.LUIS_APP_ID, query=test_request)
 
     expected_intent = "OrderTravelIntent"
     actual_intent = test_response.top_scoring_intent.intent
     assert actual_intent == expected_intent
-
-    """
-    runtime_credentials = CognitiveServicesCredentials(CONFIG.LUIS_API_KEY)
-    client_runtime = LUISRuntimeClient(endpoint=CONFIG.LUIS_API_ENDPOINT, credentials=runtime_credentials)
-
-    test_request = { "query" : "I need to book a flight" }
-    test_response = client_runtime.prediction.get_slot_prediction(CONFIG.LUIS_APP_ID, "Production", test_request)
-
-    expected_intent = "OrderTravelIntent"
-    actual_intent = test_response.prediction.top_intent
-
-    assert actual_intent == expected_intent
-    """
