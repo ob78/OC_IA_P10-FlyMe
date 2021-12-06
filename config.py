1c1b4368fd9ef5da8e0e79e4417b55f54361de48
ob78 <61279321+ob78@users.noreply.github.com>
2021-12-07 00:19:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class DefaultConfig:
     LUIS_APP_ID = "52c42e40-1395-47e1-9fcb-d8e59de2d71b"
     LUIS_API_KEY = "87488d948aba4eb39f8986e82d88d0a2"
     LUIS_API_HOST_NAME = "francecentral.api.cognitive.microsoft.com"
-    #LUIS_API_ENDPOINT = "https://flyme-luis.cognitiveservices.azure.com/"
-    LUIS_API_ENDPOINT = "francecentral.api.cognitive.microsoft.com"
+    LUIS_API_ENDPOINT = "https://flyme-luis.cognitiveservices.azure.com/"
+    #LUIS_API_ENDPOINT = "francecentral.api.cognitive.microsoft.com"
     
     #APP INSIGHTS CONFIGURATION 
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get("AppInsightsInstrumentationKey", "bc43e9d0-ee04-471f-a00b-db18c00e73a0")
