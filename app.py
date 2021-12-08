b378e4742e1c49625e0796a41bf6e04a9d445992
ob78 <61279321+ob78@users.noreply.github.com>
2021-12-08 15:23:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ TELEMETRY_CLIENT = ApplicationInsightsTelemetryClient(
 )
 
 # Create dialogs and Bot
-RECOGNIZER = FlightBookingRecognizer(CONFIG)
-BOOKING_DIALOG = BookingDialog()
+RECOGNIZER = FlightBookingRecognizer(CONFIG, telemetry_client=TELEMETRY_CLIENT)
+BOOKING_DIALOG = BookingDialog(telemetry_client=TELEMETRY_CLIENT)
 DIALOG = MainDialog(RECOGNIZER, BOOKING_DIALOG, telemetry_client=TELEMETRY_CLIENT)
 BOT = DialogAndWelcomeBot(CONVERSATION_STATE, USER_STATE, DIALOG, TELEMETRY_CLIENT)
 
