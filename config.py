8ca784da036575acfbad62b68ba09c2b899e65f6
ob78 <61279321+ob78@users.noreply.github.com>
2021-12-12 23:27:35 +0100

    Update config.py

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ class DefaultConfig:
 
     #WEB APP CONFIGURATION
     PORT = 8000
-    APP_ID = os.environ.get("MicrosoftAppId", "398fdc60-c00c-4f64-bb50-c601a6a5a254")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "n(t2N*@#fW=Ja$e5ONizvWX&")
+    APP_ID = os.environ.get("MicrosoftAppId")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword")
     
     #LUIS APP CONFIGURATION
-    LUIS_APP_ID = os.environ.get("LuisAppId", "52c42e40-1395-47e1-9fcb-d8e59de2d71b")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "87488d948aba4eb39f8986e82d88d0a2")
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "francecentral.api.cognitive.microsoft.com")
-    LUIS_API_ENDPOINT = os.environ.get("LuisAPIEndPoint", "https://flyme-luis.cognitiveservices.azure.com/")
+    LUIS_APP_ID = os.environ.get("LuisAppId")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName")
+    LUIS_API_ENDPOINT = os.environ.get("LuisAPIEndPoint")
     
     #APP INSIGHTS CONFIGURATION 
-    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get("AppInsightsInstrumentationKey", "bc43e9d0-ee04-471f-a00b-db18c00e73a0")
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get("AppInsightsInstrumentationKey")
