40ec3a58e01f515fa02d4590e9b9e17b3ff45d32
ob78 <61279321+ob78@users.noreply.github.com>
2021-12-12 16:18:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -82,18 +82,13 @@ async def messages(req: Request) -> Response:
 
 
 def init_func(argv):
-    APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
-    
-    #APP = web.Application(middlewares=[aiohttp_error_middleware, bot_telemetry_middleware])
-    #APP = web.Application(middlewares=[aiohttp_error_middleware])
-    
+    APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])    
     APP.router.add_post("/api/messages", messages)
     return APP
 
 if __name__ == "__main__":
     APP = init_func(None)
     try:
-        #web.run_app(APP, host="localhost", port=CONFIG.PORT)
         web.run_app(APP, host="0.0.0.0", port=CONFIG.PORT)
     except Exception as error:
         raise error
